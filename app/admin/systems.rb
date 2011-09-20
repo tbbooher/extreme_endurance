f189de54f8b0eed7cbc3455570b52e1cda22181b
Tim Booher <ads@theboohers.org>
2011-09-20 18:23:00 +0000

    index changes

--- app/admin/systems.rb
+++ app/admin/systems.rb
@@ -1,5 +1,9 @@
 ActiveAdmin.register System do
   index do
+    column "Name", :name
     column "Service Ceiling (ft)", :service_ceiling
+    column "Max Speed (kts)", :max_speed
+    column "Endurance (days)", :endurance
+    column "Payload (lbs)", :payload
   end
 end
