2be3bff2afc7050f2521fb5239d8f8350e7ac24c
Tim on tacitus <tim@tacitus.com>
2011-09-20 12:52:10 -0400

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110920150356) do
+ActiveRecord::Schema.define(:version => 20110920152423) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(:version => 20110920150356) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "links", :force => true do |t|
+    t.string   "title"
+    t.string   "link"
+    t.text     "notes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "systems", :force => true do |t|
     t.integer  "max_speed"
