490e7932068f330e3ca3c6e96c42d49756886184
Tim Booher <ads@theboohers.org>
2011-09-20 22:01:29 +0000

    testing

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -2,9 +2,12 @@ alert("running");
 
 var data = [4,8,15,16,23,42];
 
-var chart = d3.select("#chart");
+var chart = d3.select("body")
+   .append("div")
+     .attr("class", "chart");
 
 chart.selectAll("div")
-  .data(data)
-  .enter().append("div").style("width", function(d) { return d*10 + "px";})
-  .text(function(d) { return d; });
+     .data(data)
+   .enter().append("div")
+     .style("width", function(d) { return d * 10 + "px"; })
+     .text(function(d) { return d; });
