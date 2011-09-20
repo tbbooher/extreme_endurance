e54e06aa5b844dc76b9ca299db51522a9ab8ab03
Tim on tacitus <tim@tacitus.com>
2011-09-20 11:08:14 -0400

    this is with active admin

---
+++ app/admin/systems.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register System do
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Extreme::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :systems
 
   #get \"users\/show\"
